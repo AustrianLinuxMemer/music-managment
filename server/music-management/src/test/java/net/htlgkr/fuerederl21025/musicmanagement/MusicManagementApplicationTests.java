b8448c74f516acee7e103ce40b9f3b4d007d9f22
Leo Füreder <leo.fuereder@outlook.com>
2024-04-24 14:32:20 +0200

    music-management neu hinzugefügt

---
+++ server/music-management/src/test/java/net/htlgkr/fuerederl21025/musicmanagement/MusicManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package net.htlgkr.fuerederl21025.musicmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
